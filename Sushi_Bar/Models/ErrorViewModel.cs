c3f9ce9efb44eb6ddb48a102b56aab0ea5760c37
husiwijn-L <0.lera.ded.0@gmail.com>
2025-05-23 10:30:04 +0300

    Добавьте файлы проекта.

---
+++ Sushi_Bar/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Sushi_Bar.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
